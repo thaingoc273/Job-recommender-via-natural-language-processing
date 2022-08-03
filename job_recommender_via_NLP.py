7da1bbf116dacf40f1c9a04af55e55d67987de16
NGOC NGUYEN <thaingoc273@gmail.com>
2022-08-03 13:26:00 +0200

    add

--- job_recommender_via_NLP.py
+++ job_recommender_via_NLP.py
@@ -45,24 +45,45 @@ from io import StringIO
 import textract
 import docx2txt
 import pdfplumber
+import pickle
 
 def main():   
     st.title("Job Recommender via Natural Language Processing")
     
-    uploaded_file = st.sidebar.file_uploader('Please upload CV in .pdf or .docx file', type=["pdf","docx"])
-    if uploaded_file is not None:
-        if (uploaded_file.type=='application/pdf'):
-            CV_pdf = pdfplumber.open(uploaded_file)
-            pageObj = CV_pdf.pages[0]
-            CV_text = pageObj.extract_text()
-            st.text_area(CV_text)
-        elif ((uploaded_file.type=='application/vnd.openxmlformats-officedocument.wordprocessingml.document')|(uploaded_file.type=='application/msword')):
-            # CV_text = textract.process(uploaded_file)
-            # CV_text = CV_text.decode("utf-8") 
-            # st.text_area(CV_text)            
-            CV_text = docx2txt.process(uploaded_file)
-            st.text_area(CV_text)
-        #st.text(uploaded_file.type)
+    upload_file = st.sidebar.file_uploader('Please upload CV in .pdf or .docx file', type=["pdf","docx"])
+    if upload_file is not None:
+        CV_text = load_file(upload_file, upload_file.type)
+        #st.text_area(CV_text)
+    #skill_extractor = pickle.load(open('model/skill_extractor.pkl','rb'))
+        CV_skill = skill_extraction_one(CV_text)
+        CV_skill_text = ', '.join(CV_skill)
+        st.sidebar.title('Your skills')
+        st.sidebar.text_area(CV_skill_text)
+        
+def load_file(upload_file, typ):
+    if (typ != 'application/pdf') & (typ != 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'):
+        st.error('File is not in correct format. Please upload again')
+        return None
+    
+    elif (typ=='application/pdf'):
+        CV_pdf = pdfplumber.open(upload_file)
+        num_page = CV_pdf.pages
+        CV_text = ''
+        for i in range(len(num_page)):
+            pageObj = CV_pdf.pages[i]
+            CV_text += pageObj.extract_text()
+        return CV_text
+    elif (typ == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'):
+        CV_text = docx2txt.process(upload_file)
+        return CV_text
+    
+def skill_extraction_one(text):
+    df_skill_ngram = pd.json_normalize(skill_extractor.annotate(text)['results']['ngram_scored'])
+    df_skill_full_match = pd.json_normalize(skill_extractor.annotate(text)['results']['full_matches'])
+    df_skill = pd.concat([df_skill_ngram, df_skill_full_match])
+    #df_skill['doc_node_value'].drop_duplicates(inplace=True)
+    return df_skill['doc_node_value'].unique().tolist()
 
 if __name__ == "__main__":
+    skill_extractor = pickle.load(open('model/skill_extractor.pkl','rb'))
     main()
