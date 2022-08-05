0786c63b5c6020ec9f1a631b674fad6ce9d754b5
NGOC NGUYEN <thaingoc273@gmail.com>
2022-08-05 19:19:55 +0200

    add full name skill

--- job_recommender_via_NLP.py
+++ job_recommender_via_NLP.py
@@ -82,16 +82,17 @@ def main():
         else:
             df = df_job.copy()
         
-        st.sidebar.markdown("## Choose type of algorithms")
-        algorithm = st.sidebar.selectbox("", ["Cosine", "KNN", "Pearson"])
+#         st.sidebar.markdown("## Choose type of algorithms")
+#         algorithm = st.sidebar.selectbox("", ["Cosine", "KNN", "Pearson"])
         
-        if (algorithm=='Cosine'):
-            top_job =  cosin_similarity(df, cv_skill_text, number)
-        elif (algorithm=='KNN'):
-            top_job = KNN_similartity(df, cv_skill_text, number)
-        else:
-            top_job =  person_corr_similarity(df, cv_skill_text, number)
+#         if (algorithm=='Cosine'):
+#             top_job =  cosin_similarity(df, cv_skill_text, number)
+#         elif (algorithm=='KNN'):
+#             top_job = KNN_similartity(df, cv_skill_text, number)
+#         else:
+#             top_job =  person_corr_similarity(df, cv_skill_text, number)
       
+        top_job = cosin_similarity(df, cv_skill_text, number)
         
         st.sidebar.text_area('Your skill', cv_skill_text)
         top_job = top_job.reset_index()
@@ -135,6 +136,7 @@ def load_file(upload_file, typ):
         #     pageObj = CV_pdf.pages[i]
         #     CV_text += pageObj.extract_text()
         # return CV_text
+        
     elif (typ == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'):
         CV_text = docx2txt.process(upload_file)
         return CV_text
@@ -152,8 +154,9 @@ def skill_extraction_one(text):
         df_skill = df_skill_full_match
     # df_skill_full_match = pd.json_normalize(skill_extractor.annotate(text)['results']['full_matches'])
     # df_skill = pd.concat([df_skill_ngram, df_skill_full_match])
+    df_skill = df_skill.merge(df_skill_data, how = 'left', left_on='skill_id', right_on='skill_id')
     
-    return df_skill['doc_node_value'].unique().tolist()
+    return df_skill['skill_name'].unique().tolist()
 
 
 def skill_extractor_model():
@@ -204,19 +207,36 @@ def person_corr_similarity(df, cv_skill_text, number):
     df['pearson_corr'] = lst_pearson_corr
     
     return df.sort_values(by='pearson_corr', ascending=False).head(number)
-    
-if __name__ == "__main__":
-    number = 15
-    nlp = spacy.load("en_core_web_sm")
-    skill_extractor = SkillExtractor(nlp, SKILL_DB, PhraseMatcher)
-    # model = pickle.load(open('model/skill_extractor_sm.pkl','rb'))
-    tfidf_model = pickle.load(open('model/tfidf_model.pkl','rb'))
+
+@st.cache
+def load_dataframe():
     df_job = pd.read_csv('data/skill_extraction_Skiller_03.08_final_web_v1.csv')
     df_job_en = df_job.loc[df_job['language']=='en'].copy()
     df_job_de = df_job.loc[df_job['language']=='de'].copy()
+    df_skill_data = pd.read_csv('data/skill_name.csv') # read all skills
+    return df_job, df_job_en, df_job_de, df_skill_data
+
     
-    st.set_page_config(layout="wide")    
-    # st.title("Job Recommender via Natural Language Processing")
+if __name__ == "__main__":
+    number = 15
+    st.set_page_config(layout="wide")
+    
+    nlp = spacy.load("en_core_web_sm")
+    skill_extractor = SkillExtractor(nlp, SKILL_DB, PhraseMatcher)
+    
+    # model = pickle.load(open('model/skill_extractor_sm.pkl','rb'))
+    
+    tfidf_model = pickle.load(open('model/tfidf_model.pkl','rb'))
+    
+    
+    df_job, df_job_en, df_job_de, df_skill_data = load_dataframe()
+    
+    # df_job = pd.read_csv('data/skill_extraction_Skiller_03.08_final_web_v1.csv')
+    # df_job_en = df_job.loc[df_job['language']=='en'].copy()
+    # df_job_de = df_job.loc[df_job['language']=='de'].copy()
+    # df_skill_data = pd.read_csv('data/skill_name.csv') # read all skills
+    
+        
     col1, col2 = st.columns(2)
     with col1:
         st.image('picture/logo_WBS.png')
